20a2b90d0e531e08be5b86e22db2bdd12fbe2bb8
kevin <kevin@avnet.com>
2021-06-26 22:54:30 +0800

    修改好 uvmcopy: fork 新进程时，子进程和父进程共享 p->sz 的内存区域, 除了 pagetable, PTE, TRAPFRAME 是单独分配外，其他都是共享的. 所以做如下修改: 1. uvmcopy 不再给新进程分配内存, 而是将父进程的再映射到新进程 2. 父进程和子进程的 PTE 都要设置为只读，而且标记有 RSW 项. 3. 当子进程因缺乏内存而无法映射时，回退时不能析放与夫进程共享的内存.

--- kernel/vm.c
+++ kernel/vm.c
@@ -311,7 +311,6 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -319,19 +318,17 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
+    *pte &= ~PTE_W;
+    *pte |= PTE_RSW;
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
       goto err;
     }
   }
   return 0;
 
  err:
-  uvmunmap(new, 0, i / PGSIZE, 1);
+  uvmunmap(new, 0, i / PGSIZE, 0);
   return -1;
 }
 
