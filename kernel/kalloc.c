8e083c3d81e1702896bf1b355734e9c5ba7dbc8e
kevin <kevin@avnet.com>
2021-06-29 10:11:22 +0800

    增加 page_ref 以及原子操作.

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,10 +23,40 @@ struct {
   struct run *freelist;
 } kmem;
 
+struct {
+  struct spinlock lock;
+  int ref[PHYSTOP/PGSIZE];
+} page_ref;
+
+// op: 0 -> read, 1 -> inc, 2-> dec, other -> unknown op
+int
+page_ref_op(int op, uint64 pa)
+{
+  int value = 0;
+  acquire(&page_ref.lock);
+  switch(op){
+    case 0:
+      value = page_ref.ref[pa/PGSIZE];
+      break;
+    case 1:
+      page_ref.ref[pa/PGSIZE] += 1;
+      break;
+    case 2:
+      page_ref.ref[pa/PGSIZE] -= 1;
+      break;
+    default:
+      panic("page_ref_op, unknown op");
+  }
+  release(&page_ref.lock);
+  return value;
+}
+
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&page_ref.lock, "page_ref");
+  memset(page_ref.ref, 0, sizeof(page_ref.ref));
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +65,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    page_ref_op(1, (uint64)p);
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,15 +83,18 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+  page_ref_op(2, (uint64)pa);
+  if(page_ref_op(0, (uint64)pa) == 0){
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+    r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+  }
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -76,7 +111,9 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
+  if(r){
     memset((char*)r, 5, PGSIZE); // fill with junk
+    page_ref_op(1, (uint64)r);
+  }
   return (void*)r;
 }
